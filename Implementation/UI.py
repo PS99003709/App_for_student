182fbaed5605b04ddd37aef22a0e2ea8e420c26b
99003613 <yash62430@gmail.com>
2021-03-09 11:38:44 +0530

    Initial Ui

--- Implementation/UI.py
+++ Implementation/UI.py
@@ -1,19 +1,19 @@
-# import tkinter as tk
-# from tkinter import *
-# from tkinter import ttk  
-# from automailer import *
-# from NEW_RADAR import *
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk  
+from automailer import *
+from NEW_RADAR import *
 
-# def sel1():
+def sel1():
     
-#     selection = "You selected Python as a course "
+    selection = "You selected Python as a course "
 
-#     btn1 = Button(root, text = 'Send Mail to Students !', bd = '5',command =send_mail_student  )
-#     btn1.pack(side = 'top',ipadx =5 , ipady =5 , padx =5 , pady =5)
+    btn1 = Button(root, text = 'Send Mail to Students !', bd = '5',command =send_mail_student  )
+    btn1.pack(side = 'top',ipadx =5 , ipady =5 , padx =5 , pady =5)
 
-#     btn2 = Button(root, text = 'Send Mail to Faculty !', bd = '5', command =send_data_to_teacher )
-#     btn2.pack(side = 'top',ipadx =5 , ipady =5 , padx =5 , pady =5)    
-#     label.config(text = selection)
+    btn2 = Button(root, text = 'Send Mail to Faculty !', bd = '5', command =send_data_to_teacher )
+    btn2.pack(side = 'top',ipadx =5 , ipady =5 , padx =5 , pady =5)    
+    label.config(text = selection)
  
 
 # root = Tk()
