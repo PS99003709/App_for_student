f36151381250067798d38365731a5ad4bbf83fc8
PS99003709 <78997691+PS99003709@users.noreply.github.com>
2021-03-09 12:39:42 +0530

    Update ClassRoom.py
    ClassRoom Top_bottom_performers for chart

--- Implementation/ClassRoom.py
+++ Implementation/ClassRoom.py
@@ -46,64 +46,64 @@ def avg():
     return avg_list
 
 
-# # Sum of rows for calculating the average marks of the students
-# def sum_of_row():
-#     res = []
-#     post_survey_sum = 0
-#     post_test_sum = 0
-#     pre_test_sum = 0
-#     pre_survey_sum = 0
-#     post_survey_row_sum = []
-#     pre_survey_row_sum = []
-#     post_test_row_sum = []
-#     pre_test_row_sum = []
-#     for i in range(0, 10):
-#         for j in range(4, 10):
-#             pre_survey_sum += pre_survey.iloc[i, j]
-#             pre_test_sum += pre_test.iloc[i, j]
-#             post_survey_sum += post_survey.iloc[i, j]
-#             post_test_sum += post_test.iloc[i, j]
-#         post_survey_row_sum.append(post_survey_sum)
-#         pre_survey_row_sum.append(pre_survey_sum)
-#         post_test_row_sum.append(post_test_sum)
-#         pre_test_row_sum.append(pre_test_sum)
-#         post_survey_sum = 0
-#         pre_test_sum = 0
-#         pre_survey_sum = 0
-#         post_test_sum = 0
-#     res.append(pre_survey_row_sum)
-#     res.append(pre_test_row_sum)
-#     res.append(post_survey_row_sum)
-#     res.append(post_test_row_sum)
-#     return res
+# Sum of rows for calculating the average marks of the students
+def sum_of_row():
+    res = []
+    post_survey_sum = 0
+    post_test_sum = 0
+    pre_test_sum = 0
+    pre_survey_sum = 0
+    post_survey_row_sum = []
+    pre_survey_row_sum = []
+    post_test_row_sum = []
+    pre_test_row_sum = []
+    for i in range(0, 10):
+        for j in range(4, 10):
+            pre_survey_sum += pre_survey.iloc[i, j]
+            pre_test_sum += pre_test.iloc[i, j]
+            post_survey_sum += post_survey.iloc[i, j]
+            post_test_sum += post_test.iloc[i, j]
+        post_survey_row_sum.append(post_survey_sum)
+        pre_survey_row_sum.append(pre_survey_sum)
+        post_test_row_sum.append(post_test_sum)
+        pre_test_row_sum.append(pre_test_sum)
+        post_survey_sum = 0
+        pre_test_sum = 0
+        pre_survey_sum = 0
+        post_test_sum = 0
+    res.append(pre_survey_row_sum)
+    res.append(pre_test_row_sum)
+    res.append(post_survey_row_sum)
+    res.append(post_test_row_sum)
+    return res
 
 
-# # top five performers of the class
-# def top_five(k):
-#     top5 = []
-#     sum_rows = sum_of_row()
-#     sum_rows = sum_rows[k]
-#     sum_rows.sort()
-#     sum_rows.reverse()
-#     j = 0
-#     for i in range(len(sum_rows)):
-#         j = sum_rows[i]
-#         top5.append(j)
-#         if i == 4:
-#             break
-#     return top5
+# top five performers of the class
+def top_five(k):
+    top5 = []
+    sum_rows = sum_of_row()
+    sum_rows = sum_rows[k]
+    sum_rows.sort()
+    sum_rows.reverse()
+    j = 0
+    for i in range(len(sum_rows)):
+        j = sum_rows[i]
+        top5.append(j)
+        if i == 4:
+            break
+    return top5
 
 
-# # Bottom five performer of class
-# def bottom_five(k):
-#     bottom5 = []
-#     sum_rows = sum_of_row()
-#     sum_rows = sum_rows[k]
-#     sum_rows.sort()
-#     j = 0
-#     for i in range(len(sum_rows)):
-#         j = sum_rows[i]
-#         bottom5.append(j)
-#         if i == 4:
-#             break
-#     return bottom5
+# Bottom five performer of class
+def bottom_five(k):
+    bottom5 = []
+    sum_rows = sum_of_row()
+    sum_rows = sum_rows[k]
+    sum_rows.sort()
+    j = 0
+    for i in range(len(sum_rows)):
+        j = sum_rows[i]
+        bottom5.append(j)
+        if i == 4:
+            break
+    return bottom5
